cfd9b66d86910071f1f4274ed7899ec0e1f3b2dc
Shining143 <ahmadslb2293@gmail.com>
2023-03-26 05:16:29 +0530

    sampleProject

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,26 @@
+package tests;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class Test1 {
+	
+	@Test
+	public void method1() {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		RemoteWebDriver driver = new ChromeDriver(co);
+		driver.get("https://mvnrepository.com/");
+	
+	}
+
+}
